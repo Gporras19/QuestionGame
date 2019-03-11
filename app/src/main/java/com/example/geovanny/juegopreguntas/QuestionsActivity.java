c535547ded9c92622648e51ff5421a118b8f67b5
Gporras19 <47360602+Gporras19@users.noreply.github.com>
2019-03-11 14:02:37 -0600

    Actualizacion App
    Ultima actualizacion de la app de preguntas

---
+++ app/src/main/java/com/example/geovanny/juegopreguntas/QuestionsActivity.java
@@ -0,0 +1,16 @@
+
+package com.example.geovanny.juegopreguntas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuestionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questions);
+
+        TextView
+    }
+}
